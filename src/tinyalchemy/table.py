8b898aea0081ed47488f4b8a4fde226439356c14
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2023-01-04 15:02:23 -0500

    add primary_keys property getter and setter to TintySqlTable

--- src/tinyalchemy/table.py
+++ src/tinyalchemy/table.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from tinytable import Table
 import tabulize
 from tinytim.rows import row_dicts_to_data
@@ -9,6 +10,14 @@ class TinySqlTable(Table):
         self.sqltable = tabulize.SqlTable(name, engine)
         data = row_dicts_to_data(self.sqltable.old_records)
         super().__init__(data)
+
+    @property
+    def primary_keys(self) -> list[str]:
+        return self.sqltable.primary_keys
+
+    @primary_keys.setter
+    def primary_keys(self, column_names: Sequence[str]) -> None:
+        self.sqltable.primary_keys = list(column_names)
 
     def pull(self):
         self.sqltable.pull()
